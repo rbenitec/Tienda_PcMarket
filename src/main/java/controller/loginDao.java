9125f5fdebc2f38f03612e7a4412d4baec44a169
Fabricio <fabricio.gu.ju@gmail.com>
2023-05-31 01:19:58 -0500

    update

--- src/main/java/controller/loginDao.java
+++ src/main/java/controller/loginDao.java
@@ -13,25 +13,17 @@ public class loginDao {
     static Usuario validarUsuario(String username, String password) {
     Usuario p = null;
     try (Connection cn = MySQLConexion.getConexion()) {
-        String sql = "SELECT id, roles_id, email, password, nombres, sexo, fecnacimiento, telefono, direccion, estado, dni, username FROM usuarios WHERE username = ? AND password = ?";
+        String sql = "SELECT username, password FROM usuarios WHERE username = ? AND password = ?";
         try (PreparedStatement st = cn.prepareStatement(sql)) {
             st.setString(1, username);
             st.setString(2, password);
             try (ResultSet rs = st.executeQuery()) {
                 if (rs.next()) {
                     p = new Usuario();
-                    p.setId(rs.getString(1));
-                    p.setId_rol(rs.getInt(2));
-                    p.setEmail(rs.getString(3));
-                    p.setPassword(rs.getString(4));
-                    p.setNombres(rs.getString(5));
-                    p.setSexo(rs.getString(6));
-                    p.setFecnacimiento(rs.getString(7));
-                    p.setTelefono(rs.getString(8));
-                    p.setDireccion(rs.getString(9));
-                    p.setEstado(rs.getString(10));
-                    p.setDni(rs.getString(11));
-                    p.setUsername(rs.getString(12));
+                    p.setUsername(rs.getString(1));
+                    p.setPassword(rs.getString(2));
+                    
+                    
                 }
             }
         }
